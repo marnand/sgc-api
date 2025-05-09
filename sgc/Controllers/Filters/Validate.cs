70b7c30cbc19393938e36aaf19837cdfeee546b0
marnand <marnandf@gmail.com>
2025-05-09 00:13:47 -0300

    add gitignore

---
+++ sgc/Controllers/Filters/Validate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace sgc.Controllers.Filters;
+
+public class Validate : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        //var headerToken = filterContext.HttpContext.Request.Headers["Authorization"];
+        //var tokenUser = JWTUtil.ValidateToken(headerToken!);
+        //if (tokenUser == null || tokenUser.Id == 0)
+        //{
+        //    filterContext.HttpContext.Response.StatusCode = 401;
+        //    throw new Exception("Token Inválido!");
+        //}
+    }
+}
